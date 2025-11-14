db53b6f6ed6c20e2abdcd11142ce6358a32523d5
sai <sainitinyadav.mudike@gmail.com>
2025-11-14 16:58:50 -0500

    add turtles to the start point

--- main.py
+++ main.py
@@ -1,7 +1,24 @@
 from turtle import Turtle,Screen
 
-t1=Turtle()
-
 
 screen=Screen()
+
+screen.setup(width=500,height=400)
+colors=["red",'yellow','blue','green','orange','purple']
+
+
+user_bet=screen.textinput('Make Your Bet','Which turtle will win the race? Enter a color: ')
+
+x_pos=-235
+y_pos=200
+
+for i in range(6):
+    y_pos-=55
+    turtle=Turtle(shape="turtle")
+    turtle.color(colors[i])
+    turtle.penup()
+    turtle.goto(x_pos,y_pos)
+
+
+
 screen.exitonclick()
